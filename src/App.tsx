1ba91c710c4ddfb3e0950bc7c6776f4290684704
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-07-29 01:33:10 +0400

    feat(App): add some components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Instructions from "./components/Instructions";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box display="flex">
+      <Box width="100%">
+        <Header />
+        <Container maxW="container.lg">
+          <Main />
+        </Container>
+        <Footer />
+      </Box>
+      <Instructions />
+    </Box>
   );
 }
 
