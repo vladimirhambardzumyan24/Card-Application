6b4fab2a81c3941f86f3fb0ee970e4069f700532
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-07-29 01:30:06 +0400

    feat(TextConstants): create constants file and add some text constants

---
+++ src/constants/TextConstants.ts
@@ -0,0 +1,16 @@
+export enum CardValues {
+    MAX=1000
+} 
+
+export enum ButtonTexts {
+    ADD_CARD = 'ADD Card',
+    SORT_CARDS = 'Sort Cards'
+} 
+
+export enum FooternTexts {
+    FOOTER = 'Footer'
+} 
+
+export enum RightSideTexts {
+    INSTRUCTIONS = 'Press the "ADD Card" button to add the new Card. Use the "Sort Cards" button to sort the Cards by the increase. Press an X icon on the top right to delete them.'
+}
